2b7221f34ee64ca4c30bd502dd22cd8ff48f95bb
Balazs Sipos <balazs.sipos.hu@gmail.com>
2024-11-12 12:55:03 +0100

    Add dummy  header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 import { PersonalSummary } from '../components/personnel-summary/PersonnelSummary';
 import Professional from '../components/professional/Professional';
@@ -6,7 +5,6 @@ import Professional from '../components/professional/Professional';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header>Header</header>
       <main className={styles.main}>
         <section>
           <PersonalSummary />
@@ -30,50 +28,6 @@ export default function Home() {
           Between the blocks, there can be a button to show a funny quote.
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
