cc8d63f60a88e1335ea49bd52a658b9dfe85db56
Balazs Sipos <balazs.sipos.hu@gmail.com>
2024-11-13 12:24:44 +0100

    Add  Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,31 @@
+'use client';
+import Link from 'next/link';
+import Hamburger from './Hamburger';
+import { useState } from 'react';
+import styles from './header.module.css';
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles.header}>
+      {isOpen && (
+        <div className={styles['header__menu']}>
+          <Link onClick={() => setIsOpen(false)} href="/">
+            <h2>Home</h2>
+          </Link>
+          <Link onClick={() => setIsOpen(false)} href="/about">
+            <h2>About</h2>
+          </Link>
+        </div>
+      )}
+      <div className={styles['header__hamburger']}>
+        <Hamburger isOpen={isOpen} onClick={toggleMenu}></Hamburger>
+      </div>
     </header>
   );
 }
